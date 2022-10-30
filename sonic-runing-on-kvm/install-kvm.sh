6272fd063de820b4912847579a3c8b9a52f46084
cafenero <2006yusuke@gmail.com>
2022-10-31 01:34:19 +0900

    mod kvm install and xml

--- sonic-runing-on-kvm/install-kvm.sh
+++ sonic-runing-on-kvm/install-kvm.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
 # install kvm packages
 sudo apt update
@@ -8,22 +8,30 @@ sudo apt install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils 
 sudo systemctl is-active libvirtd
 sudo usermod -aG libvirt $USER
 sudo usermod -aG kvm $USER
-newgrp libvirt
-newgrp kvm
-
+echo "================================"
+echo "If you need, "
+echo "newgrp libvirt"
+echo "newgrp kvm"
+echo "================================"
 
 # copy your image here
-cp ../sonic-build/sonic-buildimage/target/sonic-vs.img.gz .
-gunzip -k sonic-vs-no-pass.img.gz
-
-# reference xml
-# wget https://raw.githubusercontent.com/sonic-net/sonic-buildimage/master/platform/vs/sonic.xml
+sudo cp ../sonic-build/sonic-buildimage/target/sonic-vs.img.gz /var/lib/libvirt/images/
+sudo gunzip -k /var/lib/libvirt/images/sonic-vs.img.gz
 
 ## create qemu
+# reference xml
+# wget https://raw.githubusercontent.com/sonic-net/sonic-buildimage/master/platform/vs/sonic.xml
 sudo virsh create sonic.xml
 
 ## check qmemu list
 sudo virsh list --all
 
 ## login
-telnet localhost 7000
+echo "================================"
+echo "SONiC login:"
+echo "admin/pass"
+echo "telnet localhost 7000"
+echo "================================"
+
+## terminate VM
+# sudo virsh destroy sonic
